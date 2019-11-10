57b73f98f4fec55922e496b0c8580f1f22e9a049
Denis Samusevich <samynchik@gmail.com>
2019-11-10 20:42:39 +0300

    Added Task002

---
+++ Task002/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Task002
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTextWeightInPounds = new System.Windows.Forms.Label();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.textBoxInputNumberPounds = new System.Windows.Forms.TextBox();
+            this.labelTextResultKilogram = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTextWeightInPounds
+            // 
+            this.labelTextWeightInPounds.AutoSize = true;
+            this.labelTextWeightInPounds.Location = new System.Drawing.Point(40, 33);
+            this.labelTextWeightInPounds.Name = "labelTextWeightInPounds";
+            this.labelTextWeightInPounds.Size = new System.Drawing.Size(82, 14);
+            this.labelTextWeightInPounds.TabIndex = 0;
+            this.labelTextWeightInPounds.Text = "Вес в фунтах";
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(240, 30);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // textBoxInputNumberPounds
+            // 
+            this.textBoxInputNumberPounds.Location = new System.Drawing.Point(130, 30);
+            this.textBoxInputNumberPounds.Name = "textBoxInputNumberPounds";
+            this.textBoxInputNumberPounds.Size = new System.Drawing.Size(100, 22);
+            this.textBoxInputNumberPounds.TabIndex = 2;
+            this.textBoxInputNumberPounds.TextChanged += new System.EventHandler(this.textBoxInputNumberPounds_TextChanged);
+            this.textBoxInputNumberPounds.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxInputNumberPounds_KeyPress);
+            // 
+            // labelTextResultKilogram
+            // 
+            this.labelTextResultKilogram.AutoSize = true;
+            this.labelTextResultKilogram.Location = new System.Drawing.Point(127, 66);
+            this.labelTextResultKilogram.Name = "labelTextResultKilogram";
+            this.labelTextResultKilogram.Size = new System.Drawing.Size(0, 14);
+            this.labelTextResultKilogram.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 173);
+            this.Controls.Add(this.labelTextResultKilogram);
+            this.Controls.Add(this.textBoxInputNumberPounds);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.labelTextWeightInPounds);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Фунты-килограммы";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTextWeightInPounds;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.TextBox textBoxInputNumberPounds;
+        private System.Windows.Forms.Label labelTextResultKilogram;
+    }
+}
+
---
+++ Task002/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task002
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            buttonOK.Enabled = false;
+        }
+
+        private void textBoxInputNumberPounds_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                return;
+            }
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }
+            if (e.KeyChar == ',')
+            {
+                if (textBoxInputNumberPounds.Text.IndexOf(',') != -1 || textBoxInputNumberPounds.Text.Length == 0)
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            if (char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                {
+                    buttonOK.Focus();
+                }
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void textBoxInputNumberPounds_TextChanged(object sender, EventArgs e)
+        {
+            labelTextResultKilogram.Text = "";
+            if (textBoxInputNumberPounds.Text.Length == 0)
+            {
+                buttonOK.Enabled = false;
+            }
+            else
+            {
+                buttonOK.Enabled = true;
+            }
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            double pounds;
+            double kg;
+
+            pounds = Math.Round(Convert.ToDouble(textBoxInputNumberPounds.Text),3);
+            kg= Math.Round(pounds* 0.453592,3);
+
+            labelTextResultKilogram.Text = pounds.ToString()+" ф. = " + kg.ToString() + " кг";
+        }
+
+    }
+}
