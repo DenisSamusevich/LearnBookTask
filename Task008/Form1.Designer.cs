ae28e9e1bf795df354c1f116cc1142864afee9e7
Denis Samusevich <samynchik@gmail.com>
2019-11-14 22:13:25 +0300

    Task008 is added

---
+++ Task008/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Task008
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxViewPictures = new System.Windows.Forms.ListBox();
+            this.buttonBrowseFolders = new System.Windows.Forms.Button();
+            this.labelFolderPath = new System.Windows.Forms.Label();
+            this.pictureBoxViewPictures = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxViewPictures)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxViewPictures
+            // 
+            this.listBoxViewPictures.FormattingEnabled = true;
+            this.listBoxViewPictures.ItemHeight = 14;
+            this.listBoxViewPictures.Location = new System.Drawing.Point(13, 48);
+            this.listBoxViewPictures.Name = "listBoxViewPictures";
+            this.listBoxViewPictures.Size = new System.Drawing.Size(120, 200);
+            this.listBoxViewPictures.TabIndex = 0;
+            this.listBoxViewPictures.SelectedIndexChanged += new System.EventHandler(this.listBoxViewPictures_SelectedIndexChanged);
+            // 
+            // buttonBrowseFolders
+            // 
+            this.buttonBrowseFolders.Location = new System.Drawing.Point(13, 300);
+            this.buttonBrowseFolders.Name = "buttonBrowseFolders";
+            this.buttonBrowseFolders.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowseFolders.TabIndex = 1;
+            this.buttonBrowseFolders.Text = "Browse";
+            this.buttonBrowseFolders.UseVisualStyleBackColor = true;
+            this.buttonBrowseFolders.Click += new System.EventHandler(this.buttonBrowseFolders_Click);
+            // 
+            // labelFolderPath
+            // 
+            this.labelFolderPath.AutoSize = true;
+            this.labelFolderPath.Location = new System.Drawing.Point(13, 13);
+            this.labelFolderPath.Name = "labelFolderPath";
+            this.labelFolderPath.Size = new System.Drawing.Size(38, 14);
+            this.labelFolderPath.TabIndex = 2;
+            this.labelFolderPath.Text = "label1";
+            // 
+            // pictureBoxViewPictures
+            // 
+            this.pictureBoxViewPictures.Location = new System.Drawing.Point(140, 48);
+            this.pictureBoxViewPictures.Name = "pictureBoxViewPictures";
+            this.pictureBoxViewPictures.Size = new System.Drawing.Size(400, 300);
+            this.pictureBoxViewPictures.TabIndex = 3;
+            this.pictureBoxViewPictures.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 361);
+            this.Controls.Add(this.pictureBoxViewPictures);
+            this.Controls.Add(this.labelFolderPath);
+            this.Controls.Add(this.buttonBrowseFolders);
+            this.Controls.Add(this.listBoxViewPictures);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Просмотр иллюстраций";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxViewPictures)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxViewPictures;
+        private System.Windows.Forms.Button buttonBrowseFolders;
+        private System.Windows.Forms.Label labelFolderPath;
+        private System.Windows.Forms.PictureBox pictureBoxViewPictures;
+    }
+}
+
---
+++ Task008/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Task008
+{
+    public partial class Form1 : Form
+    {
+        int pictureBoxWidth;
+        int pictureBoxHeight;
+        int pictureBoxCoordinateX;
+        int pictureBoxCoordinateY;
+        string folderPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBoxHeight = pictureBoxViewPictures.Height;
+            pictureBoxWidth = pictureBoxViewPictures.Width;
+            pictureBoxCoordinateX = pictureBoxViewPictures.Location.X;
+            pictureBoxCoordinateY = pictureBoxViewPictures.Location.Y;
+
+            listBoxViewPictures.Sorted = true;
+
+            DirectoryInfo directoryInfoMyPictures = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
+            folderPath = directoryInfoMyPictures.FullName;
+            labelFolderPath.Text = folderPath;
+
+            FillListBox(folderPath);
+        }
+
+        private bool FillListBox(string path)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            FileInfo[] fileInfos = directoryInfo.GetFiles("*.jpg");
+            listBoxViewPictures.Items.Clear();
+            foreach (FileInfo fileInfo in fileInfos)
+            {
+                listBoxViewPictures.Items.Add(fileInfo.Name);
+            }
+            labelFolderPath.Text = path;
+            if (fileInfos.Length == 0)
+            {
+                return false;
+            }
+            else
+            {
+                listBoxViewPictures.SelectedIndex = 0;
+                return true;
+            }
+        }
+
+        private void listBoxViewPictures_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double scaleWidth;
+            double scaleHeight;
+
+            pictureBoxViewPictures.Visible = false;
+            pictureBoxViewPictures.Left = pictureBoxCoordinateX;
+            pictureBoxViewPictures.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBoxViewPictures.Image = new Bitmap(folderPath + "\\" + listBoxViewPictures.SelectedItem.ToString());
+
+            if ((pictureBoxViewPictures.Image.Width > pictureBoxWidth) || (pictureBoxViewPictures.Image.Height > pictureBoxHeight))
+            {
+                pictureBoxViewPictures.SizeMode = PictureBoxSizeMode.StretchImage;
+                scaleWidth = (double)pictureBoxWidth / (double)pictureBoxViewPictures.Image.Height;
+                scaleHeight = (double)pictureBoxWidth / (double)pictureBoxViewPictures.Image.Height;
+                if (scaleHeight< scaleWidth)
+                {
+                    pictureBoxViewPictures.Width = Convert.ToInt16(pictureBoxViewPictures.Image.Width * scaleHeight);
+                    pictureBoxViewPictures.Height = pictureBoxHeight;
+                }
+                else
+                {
+                    pictureBoxViewPictures.Width = pictureBoxWidth;
+                    pictureBoxViewPictures.Height = Convert.ToInt16(pictureBoxViewPictures.Image.Height * scaleWidth); ;
+                }
+            }
+            pictureBoxViewPictures.Left = pictureBoxCoordinateX + (pictureBoxWidth - pictureBoxViewPictures.Width) / 2;
+            pictureBoxViewPictures.Top = pictureBoxCoordinateY + (pictureBoxHeight - pictureBoxViewPictures.Height) / 2;
+            pictureBoxViewPictures.Visible = true;
+        }
+
+        private void buttonBrowseFolders_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "Выберите папку, \n" + "в которой находятся иллюстрации";
+            folderBrowserDialog.ShowNewFolderButton = false;
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog.SelectedPath;
+                labelFolderPath.Text = folderPath;
+                if (!FillListBox(folderPath))
+                {
+                    pictureBoxViewPictures.Image = null;
+                }
+            }
+        }
+    }
+}
